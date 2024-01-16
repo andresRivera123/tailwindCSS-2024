98bb5aa8a79638db9b77b6197e857be396661c80
andresRivera123 <88514682+andresRivera123@users.noreply.github.com>
2024-01-17 09:03:58 +1400

    Fonts + measures button

--- 01-landing-deupload/tailwind.config.js
+++ 01-landing-deupload/tailwind.config.js
@@ -3,15 +3,19 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      fontFamily: {
+        "sofia-medium": "sofia-medium",
+        "sofia-regular": "sofia-regular"
+      },
       colors: {
         "dark-purple": "#0B0D0E",
         "primary-purple": "#651FFF",
         "secondary-purple": "#8466FF",
         "light-purple": "#ECECFE",
-        "gray-100": "#E2E4E9",
-        "gray-200": "#C4C7CF",
-        "gray-400": "#838696",
-        "gray-600": "#5C5F6E",
+        "grays-100": "#E2E4E9",
+        "grays-200": "#C4C7CF",
+        "grays-400": "#838696",
+        "grays-600": "#5C5F6E",
       },
     },
   },
