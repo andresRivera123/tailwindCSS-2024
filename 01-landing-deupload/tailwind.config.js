372607c7bf37d2afd1aa4e09ceeb0741bdb85440
andresRivera123 <88514682+andresRivera123@users.noreply.github.com>
2024-01-17 08:06:03 +1400

    New project Deupload: navbar

---
+++ 01-landing-deupload/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "dark-purple": "#0B0D0E",
+        "primary-purple": "#651FFF",
+        "secondary-purple": "#8466FF",
+        "light-purple": "#ECECFE",
+        "gray-100": "#E2E4E9",
+        "gray-200": "#C4C7CF",
+        "gray-400": "#838696",
+        "gray-600": "#5C5F6E",
+      },
+    },
+  },
+  plugins: [],
+};
